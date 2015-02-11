e47c267b655b13a3dc23174e6372db2bdf138127
Pipekung <genius.society@gmail.com>
2015-02-11 14:45:35 +0700

    update active menu

--- Menu.php
+++ Menu.php
@@ -9,9 +9,14 @@ use Yii;
  */
 class Menu {
     
-    public static function Active($name, $id='*') {
-    	if (Yii::$app->controller->module->id==$name && $id=='*') return true;
-    	elseif (Yii::$app->controller->module->id==$name && Yii::$app->controller->id==$id) return true;
+    public static function Active($name, $id=[], $unactive=[]) {
+    	if (in_array(Yii::$app->controller->id, $unactive) || in_array(Yii::$app->controller->module->id, $unactive)) return false;
+
+    	if (Yii::$app->controller->module->id==$name && empty($id)) return true;
+    	elseif (Yii::$app->controller->module->id==$name && in_array(Yii::$app->controller->id, $id)) return true;
+    	elseif (Yii::$app->controller->id==$name && empty($id)) return true;
+    	elseif (Yii::$app->controller->id==$name && in_array(Yii::$app->controller->action->id, $id)) return true;
+
         return false;
     }
     
