b5d18bacf131de18ae34501b0a7787fd7d285ea2
Pipekung <genius.society@gmail.com>
2015-02-18 10:21:29 +0700

    debug

--- Db.php
+++ Db.php
@@ -18,12 +18,12 @@ class Db {
 				JOIN profile t2 ON (t1.id = t2.user_id) 
 				LEFT JOIN hr_position t3 ON (t3.id = t2.position_id) 
 				LEFT JOIN hr_position_type t4 ON (t4.id = t2.position_type_id)
-				LEFT JOIN hr_leave_store t5 ON (t1.id = t5.user_id)
+				LEFT JOIN hr_leave_store t5 ON (t1.id = t5.user_id AND t5.fiscal_year = :fiscal_year)
 			')
 			->where([
 				't2.faculty_id' => empty($faculty_id) ? self::getFacultyByUser() : $faculty_id,
-				't5.fiscal_year' => empty($year) ? Common::getFiscalYear() : $year,
 			])
+			->addParams([':fiscal_year' => empty($year) ? Common::getFiscalYear() : $year])
 		;
 		$cmd = $query->createCommand();
 		$rows = $cmd->queryAll();
@@ -44,8 +44,9 @@ class Db {
 	public static function countLeaveDay($id, $user_id=null) {
 		$query = (new Query)
 			->select('SUM(t1.count_day)')
-			->from('hr_leave t1, (SELECT `id`, `leave_type_id`, `end_date` FROM `hr_leave`) t2')
-			->where('t2.id = :id AND t1.leave_type_id = t2.leave_type_id AND t1.end_date < t2.end_date', [':id' => $id])
+			->from('hr_leave t1, (SELECT `id`, `leave_type_id`, `end_date` FROM `hr_leave` WHERE `id` = :id) t2')
+			->where('t1.leave_type_id = t2.leave_type_id AND t1.end_date < t2.end_date')
+			->addParams([':id' => $id])
 		;
 		$cmd = $query->createCommand();
 		$result = $cmd->queryScalar();
